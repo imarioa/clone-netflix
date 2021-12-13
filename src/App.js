c2a3bf2aa584ba7497549602fc8217e4b6b7367e
Imario Almeida <imario.almeida22@gmail.com>
2021-12-12 22:52:03 -0300

    Clone Netflix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
+import React, { useEffect , useState} from 'react';
+import Tmdb from './Tmdb';
+import MovieRow from './components/MovieRow.js'
+import FeaturedMovie from './components/FeaturedMovie.js'
+import Header from './components/Header'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default () => {
 
-export default App;
+  const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
+
+  useEffect(() => {
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list); 
+
+      let originals = list.filter(i=>i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen]
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+
+      setFeaturedData(chosenInfo);
+    }
+
+    loadAll();
+  }, []);
+  useEffect(()=>{
+    const scrollListener = () =>{
+      if(window.scrollY > 10){
+        setBlackHeader(true)
+        
+      }else{
+        setBlackHeader(false)
+      }
+    }
+    window.addEventListener('scroll', scrollListener);
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
+
+  return (
+    <div className="page">
+
+      <Header black={blackHeader}/>
+
+      {featuredData &&
+        <FeaturedMovie item={featuredData}/>
+
+      }
+      <section className="lists">
+        {movieList.map((item, key)=>(
+          <div>
+            <MovieRow key={key} title={item.title} items={item.items}/>
+          </div>
+
+        ))}
+      </section>
+      <footer>
+        Feito com <span role="img" aria-label="coração"> Chico </span> <br/>
+        Direitos de imagem para Netflix <br/>
+        Dados pegos do site themoviedb.org
+      </footer>
+      {movieList.length <= 0 &&
+        <div className="loading">
+          <img src="https://www.rchandru.com/images/portfolio/loading.gif" alt="Carregando"/>
+        </div>
+      }
+      
+    </div>
+  )
+}
